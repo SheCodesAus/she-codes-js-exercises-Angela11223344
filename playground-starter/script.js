0440577b7f6908130628b6e2ea0e14c5f5793423
Angela11223344 <113986268+Angela11223344@users.noreply.github.com>
2023-02-01 09:47:06 +0800

    js practice exercises

--- playground-starter/script.js
+++ playground-starter/script.js
@@ -1,1 +1,20 @@
-console.log("Hello, world!");
+// function example() {
+//     console.log("Hi this is a function");
+// }
+
+// example ();
+
+// function add(x, y) {
+//     let result = x + y;
+//     console.log(result);
+// }
+
+// add(5, 3);
+// add(2, 1);
+// add(7, 8);
+
+function showName() {
+    let username = document.getElementById("name-text").value;
+    console.log(`Hi, ${username}`);
+}
+
